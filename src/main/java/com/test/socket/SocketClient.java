d293032d3ef70991367a485555287e7cc939ab37
Mr2277 <837544713@qq.com>
2018-05-01 21:41:04 +0800

    测试java io socket 如果DataInputStream 服务端没有数据  client一直阻塞监听。

---
+++ src/main/java/com/test/socket/SocketClient.java
@@ -0,0 +1,20 @@
+package com.test.socket;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class SocketClient {
+    public static final String IP_ADDR="127.0.0.1";
+    public static final int port=1234;
+    public static void main(String[] args) throws IOException {
+
+            Socket socket=null;
+            socket=new Socket(IP_ADDR,port);
+            DataInputStream dataInputStream=new DataInputStream(socket.getInputStream());
+            System.out.println("success");
+            System.out.println(dataInputStream.readUTF());
+
+    }
+}
---
+++ src/main/java/com/test/socket/SocketServer.java
@@ -0,0 +1,24 @@
+package com.test.socket;
+
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServer {
+    public static final int port=1234;
+    public void init() throws IOException {
+        ServerSocket serverSocket=new ServerSocket(port);
+        while (true){
+            Socket client=serverSocket.accept();
+            System.out.println("success");
+            DataOutputStream out=new DataOutputStream(client.getOutputStream());
+            out.writeUTF("rrrr");
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        new SocketServer().init();
+    }
+}
